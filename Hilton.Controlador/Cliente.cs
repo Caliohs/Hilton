7ce20a659251a41594edb231fa54a367a4431343
CarlosQ <caliohs@hotmail.com>
2021-04-24 14:28:56 -0600

    Creacion de Cliente Controlador

---
+++ Hilton.Controlador/Cliente.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hilton.Controlador
+{
+    public class Cliente
+    {
+        private int opc;
+        private string cedula;
+        private string nombre;
+        private string metodopago;
+        private string tipoCliente;
+        private string correo;
+        private string telefono;
+
+       
+
+        public Cliente(int opc, string cedula, string nombre, string metodopago, string tipoCliente, string correo, string telefono)
+        {
+            this.Opc = opc;
+            this.Cedula = cedula;
+            this.Nombre = nombre;
+            this.Metodopago = metodopago;
+            this.TipoCliente = tipoCliente;
+            this.Correo = correo;
+            this.Telefono = telefono;
+        }
+
+        public Cliente()
+        {
+            this.Opc = 0;
+            this.Cedula = "";
+            this.Nombre = "";
+            this.Metodopago = "";
+            this.TipoCliente = "";
+            this.Correo = "";
+            this.Telefono = "";
+        }
+
+        public int Opc { get => opc; set => opc = value; }
+        public string Cedula { get => cedula; set => cedula = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Metodopago { get => metodopago; set => metodopago = value; }
+        public string TipoCliente { get => tipoCliente; set => tipoCliente = value; }
+        public string Correo { get => correo; set => correo = value; }
+        public string Telefono { get => telefono; set => telefono = value; }
+    }
+}
--- Hilton.Vista/GestionarClientes.cs
+++ Hilton.Vista/GestionarClientes.cs
@@ -19,7 +19,7 @@ namespace Hilton.Vista
 
         private void btnAceptar_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("HOla");
+            
         }
     }
 }
